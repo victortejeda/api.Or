0978412ca45692627654860133c6b9413fc6f71f
Estudiante 1 <45402024+victortejeda@users.noreply.github.com>
2022-09-28 21:31:03 -0400

    Initial Commit

---
+++ api.Or/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  api.Or
+//
+//  Created by Victor Tejeda on 28/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ api.Or/api_OrApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_OrApp.swift
+//  api.Or
+//
+//  Created by Victor Tejeda on 28/9/22.
+//
+
+import SwiftUI
+
+@main
+struct api_OrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
